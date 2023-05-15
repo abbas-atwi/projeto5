4f452f9d870952be1ed5383720df7af655300b3c
Abbas Atwi <abbas_9589_new@hotmail.com>
2023-05-14 23:56:17 -0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,25 +27,58 @@ function produtosShow(index) {
 }
 
 // console.log(produtos[0].children.classList.add("active"));
+// selecionar todos os sliders e adiconar nome ao produto
+const arrayTitles = [
+  "iphone 6",
+  "iphone 7",
+  "iphone 8",
+  "iphone 9",
+  "iphone 10",
+  "iphone 11",
+  "iphone 12",
+  "iphone 13",
+  "iphone 14",
+];
+const h1 = document.createElement("h1");
+function addTitle(index) {
+  h1.innerText = arrayTitles[index];
+  produtos[index].appendChild(h1);
+}
+
 produtos.forEach((item, index) => {
   item.addEventListener("click", () => {
     produtosShow(index);
+    addTitle(index);
   });
 });
-
+addTitle(0);
 // pointers
 const arraySlide = [0, 100, 200];
 const pointers = document.querySelectorAll(".pointer");
+const slide = document.querySelector(".sliders");
+const slideChildren = slide.querySelectorAll(".slide");
+const slideArea = document.querySelectorAll(".slidearea img");
+const imgs = [
+  "banner-iphone-avif",
+  "banner-iphone14.jpeg",
+  "banner-iphone1414.png",
+];
 
+function slidersActive(index) {
+  slideChildren.forEach((item) => {
+    item.classList.remove("active");
+  });
+  slideChildren[index].classList.add("active");
+}
 function activePointer(index) {
   pointers.forEach((item) => {
     item.classList.remove("active");
   });
   pointers[index].classList.add("active");
-  const slide = document.querySelector(".sliders");
   const mg = "vw";
-  const sliders = arraySlide[index] === 0 ? 0 : "-";
+  const sliders = arraySlide[index] === 0 ? "" : "-";
   console.log((slide.style.marginLeft = `${sliders}${arraySlide[index]}${mg}`));
+  slidersActive(index);
 }
 pointers.forEach((item, index) => {
   item.addEventListener("click", () => {
